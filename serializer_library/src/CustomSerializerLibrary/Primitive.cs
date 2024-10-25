358e5cafdc40e56f7edcba2985f3d12ce5b17782
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-10-25 23:59:11 +0200

    serializer_library add datatypes & collection support

---
+++ serializer_library/src/CustomSerializerLibrary/Primitive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSerializerLibrary
+{
+    [Serializable]
+    public class Primitive
+    {
+        public int intVariable;
+        public float floatVariable;
+        public double doubleVariable;
+        public decimal decimalVariable;
+        public char charVariable;
+        public string stringVariable;
+        public bool boolVariable;
+        public short shortVariable;
+        public long longVariable;
+        public uint uintVariable;
+        public ushort ushortVariable;
+        public ulong ulongVariable;
+        public byte byteVariable;
+        public sbyte sbyteVariable;
+        public DateTime DateTimeVariable;
+    }
+}
